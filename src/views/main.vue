3867997327a2f3abcd3a716c35a4838ea7e63774
sunghaeJoung <shh03288@gmail.com>
2020-04-10 14:31:41 +0900

    메인페이지 레이아웃 추가 완료

--- src/views/main.vue
+++ src/views/main.vue
@@ -2,9 +2,19 @@
   <div class="main-container">
     <header></header>
     <section>
-      <div class="top-banner">배너넣을자리</div>
+      <div class="top-banner"></div>
       <div class="container">
-        <div class="category">다솔님이 만든거 가져와야지</div>
+        <div class="category-container">
+          <div class="category" v-for="(category, i) in categories" :key="i">
+            <div class="title">{{category.name}}</div>
+            <div class="line"></div>
+            <div class="card-container">
+              <img :src="category.image" />
+              <div class="content">{{category.content}}</div>
+              <button>클래스 둘러보기</button>
+            </div>
+          </div>
+        </div>
         <div class="class">
           <h2>진행중인 다양한 클래스를 만나보세요! ></h2>
           <card />
@@ -24,7 +34,7 @@
 </template>
 
 <script>
-import card from "@/components/main/card";
+import card from "@/components/card";
 
 export default {
   name: "main",
@@ -33,13 +43,29 @@ export default {
   },
   data() {
     return {
-      categorys: [
+      categories: [
         {
           id: 1,
-          name: "class",
-          image: "",
+          name: "CLASS",
+          image:
+            "https://i.pinimg.com/originals/87/70/d0/8770d03058a64bf76c19b343c735f28d.jpg",
           content:
             "유디션이 엄선한 선생님과 체계적인 커리큘럼으로 완벽하게 배우기"
+        },
+        {
+          id: 2,
+          name: "ONE DAY",
+          image:
+            "https://i.pinimg.com/originals/87/70/d0/8770d03058a64bf76c19b343c735f28d.jpg",
+          content: "삶에 필요한 기본 예능,하루만에 배우는 속성 클래스"
+        },
+        {
+          id: 3,
+          name: "MEET UP",
+          image:
+            "https://i.pinimg.com/originals/87/70/d0/8770d03058a64bf76c19b343c735f28d.jpg",
+          content:
+            "음악, 영화, 문학 모든 엔터테인먼트 장르를 아울러 함께 이야기를 나누는 곳"
         }
       ]
     };
@@ -50,6 +76,7 @@ export default {
 <style scoped lang="scss">
 .main-container {
   width: 100%;
+  color: #121212;
 
   header {
     height: 79px;
@@ -58,19 +85,88 @@ export default {
   section {
     .top-banner {
       height: 513px;
-      background-color: grey;
+      background: url("https://lh3.googleusercontent.com/proxy/7Ubrg9nLWPe75ds9BCKuei6bicQQJbc7qX5RDFrxrhyMIFMXpC8AsKZVO6Y-i8yRe4nSPasGhWIKMIKu44BbJphuLof3ULi6mXcFtTa9lp-FnA")
+        no-repeat center center;
+      background-size: cover;
     }
 
     .container {
       max-width: 1080px;
       margin: 141px auto 100px;
+      position: relative;
 
-      border: 1px solid red;
-
-      .category {
+      .category-container {
         height: 540px;
         margin-bottom: 173px;
-        border-bottom: 1px solid red;
+        display: flex;
+
+        .category {
+          margin-right: 26px;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+
+          &:nth-child(3n) {
+            margin-right: 0;
+          }
+
+          .title {
+            font-family: Gilroy;
+            font-size: 30px;
+            font-weight: 800;
+            text-align: center;
+          }
+
+          .line {
+            width: 44px;
+            height: 4px;
+            background-color: #0577ff;
+            margin: 28px 0 42px;
+          }
+
+          .card-container {
+            width: 343px;
+            height: 428px;
+            border-radius: 12px;
+            overflow: hidden;
+            background-color: #121212;
+            position: relative;
+            cursor: pointer;
+
+            img {
+              width: 100%;
+              height: 299px;
+            }
+
+            .content {
+              height: 129px;
+              font-size: 18px;
+              font-weight: bold;
+              line-height: 1.3;
+              color: #ffffff;
+              padding: 20px 56px 0 23px;
+            }
+
+            &:hover {
+              button {
+                display: block;
+                width: 253px;
+                height: 56px;
+                font-size: 16px;
+                font-weight: 900;
+                font-family: NotoSansCJKkr;
+                color: #ffffff;
+                border-radius: 4px;
+                border: none;
+                background-color: #0577ff;
+                position: absolute;
+                top: 122px;
+                right: 45px;
+                cursor: pointer;
+              }
+            }
+          }
+        }
       }
 
       .class {
@@ -85,12 +181,13 @@ export default {
 
     .bottom-banner {
       height: 564px;
+      background: url("https://lh3.googleusercontent.com/proxy/7Ubrg9nLWPe75ds9BCKuei6bicQQJbc7qX5RDFrxrhyMIFMXpC8AsKZVO6Y-i8yRe4nSPasGhWIKMIKu44BbJphuLof3ULi6mXcFtTa9lp-FnA")
+        no-repeat center center;
+      background-size: cover;
       display: flex;
       justify-content: center;
       align-items: center;
       flex-direction: column;
-
-      background-color: grey;
 
       div {
         font-size: 21px;
